990312a1d73abf6f27e4a5b193ad5551f4536c07
HyangRim <curomame@naver.com>
2021-11-24 01:02:20 +0900

    why do not work my jsconfig...

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React,{useState}from "react"
+import AppRouter from "components/Router";
+import {authService} from "fbase"
+
+function App() {
+  console.log();
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}/>
+      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+    </>
+  );
+}
+
+export default App;
